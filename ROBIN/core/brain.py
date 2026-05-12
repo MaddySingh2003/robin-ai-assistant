cf0f6f70be5371cae844052e34814638422cdd0b
MaddySingh2003 <Milansuryavanshi27@gmail.com>
2026-05-12 12:15:29 +0530

    robin

--- ROBIN/core/brain.py
+++ ROBIN/core/brain.py
@@ -1,8 +1,23 @@
 import ollama
- 
 
 def ask_api(prompt):
     response = ollama.chat(
         model="qwen2.5:3b",
-        messages=[{"role": "user", "content": prompt}],)
+        messages=[{
+            "role": "system",
+            "content":"""
+            Your name is Robin, you are smart local AI assistant like Jarvis,siri,alexa.
+            keep responses short, natural,and helpful.
+            Talk like professional assistant.
+            if someone ask your name,say your name is Robin.
+
+"""
+        },
+        {
+            "role": "user",
+            "content": prompt
+        }
+        ]
+ 
+            )
     return response["message"]["content"]
