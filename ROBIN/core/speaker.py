11529d309da78e4c5438e39c24d293e9e69f73ac
MaddySingh2003 <Milansuryavanshi27@gmail.com>
2026-05-14 12:32:31 +0530

    piper

--- ROBIN/core/speaker.py
+++ ROBIN/core/speaker.py
@@ -1,21 +1,38 @@
-import pyttsx3
+import subprocess
+import os
+import time
+from playsound import playsound
 
+PIPER_PATH=r"D:\AI-Assistant\piper\piper.exe"
+MODEL_PATH=r"D:\AI-Assistant\piper\models\en_us-amy-medium.onnx"
+
+OUTPUT_FILE="voice.wav"
 
 def speak(text):
-    print("ROBIN:",text)
 
-    engine=pyttsx3.init()
+    print("ROBIN:", text)
+    
+    process=subprocess.Popen(
+        [
+            PIPER_PATH,
+            "--model",
+            MODEL_PATH,
+            "--output_file",
+            OUTPUT_FILE,
+        ],
+        stdin=subprocess.PIPE,
+        text=True
+    )
 
+    process.communicate(text)
 
-    engine.setProperty("rate",170)
-    engine.setProperty("volume",1.0)
+    playsound(OUTPUT_FILE)
 
-    voices=engine.getProperty("voices")
+    time.sleep(0.5)
 
-    if len(voices)>1:
-        engine.setProperty("voice",voices[1].id)
-   
-    engine.say(text)
-    engine.runAndWait()
-
-    engine.stop()
+    try:
+        if os.path.exists(OUTPUT_FILE):
+            os.remove(OUTPUT_FILE)
+    
+    except PermissionError:
+           pass
